5565db0507977c6f39d2d387d211f5f00c616d5c
unknown <marko.sekulic30@gmail.com>
2022-01-18 11:45:49 +0100

    Add basic scene
    Basic scene includes:
    -Canvas (from react-three-fiber)
    -Lighting
    -Perspective Camera with Orbit Controls (from drei)
    -Basic box mesh

--- src/components/Canvas/Canvas.tsx
+++ src/components/Canvas/Canvas.tsx
@@ -1,7 +1,25 @@
+import { Canvas } from '@react-three/fiber';
+import { PerspectiveCamera, OrbitControls, Stats } from '@react-three/drei';
+//import canvas wrapper
 import { CanvasWrapper } from './Canvas.css';
 
 const CanvasViewer = () => {
-  return <CanvasWrapper></CanvasWrapper>;
+  return (
+    <CanvasWrapper>
+      <Canvas>
+        <color attach="background" args={['white']} />
+        <ambientLight intensity={0.3} />
+        <pointLight color={'yellow'} />
+        <PerspectiveCamera makeDefault position={[-9, 5, 10]} />
+        <OrbitControls />
+        <mesh>
+          <boxBufferGeometry attach="geometry" args={[2, 2, 2]} />
+          <meshStandardMaterial attach="material" color={'red'} />
+        </mesh>
+        <Stats />
+      </Canvas>
+    </CanvasWrapper>
+  );
 };
 
 export default CanvasViewer;
