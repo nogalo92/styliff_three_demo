2cd200954ef6563cb1c152ff2e42746f5159975b
unknown <marko.sekulic30@gmail.com>
2022-01-18 15:41:57 +0100

    Changed default number of spheres
    -Defined state for numberOfSphere
    -Default number is 500

--- src/components/Canvas/Canvas.tsx
+++ src/components/Canvas/Canvas.tsx
@@ -6,18 +6,31 @@ import { PerspectiveCamera, OrbitControls, Stats } from '@react-three/drei';
 import { CanvasWrapper } from './Canvas.css';
 import Box from '../Box/Box';
 import Sphere from '../Sphere/Sphere';
+import Slider from '../helpers/Slider/Slider';
 
 const CanvasViewer = () => {
+  //box dimensions state
   const [dimensions, setDimension] = useState<number[]>([0, 0, 0]);
+  //walls (obstacles) group state
   const [wallsGroup, setWalls] = useState<Group>(null!);
   const [isReady, setIsReady] = useState(false);
+
+  const [numberOfSpheres, setNumberOfSpheres] = useState(500);
 
   const handleDimensions = (value: number[]) => {
     setDimension(value);
     setIsReady(true);
   };
+
+  const handleNumberOfSpheres = (value: number) => {
+    setNumberOfSpheres(value);
+  };
   return (
     <CanvasWrapper>
+      <Slider
+        numberOfSpheres={numberOfSpheres}
+        setNumberOfSpheres={handleNumberOfSpheres}
+      />
       <Canvas>
         <color attach="background" args={['black']} />
         <ambientLight intensity={0.5} />
@@ -30,7 +43,7 @@ const CanvasViewer = () => {
           <Box setDimensions={handleDimensions} setWalls={setWalls}>
             {isReady && (
               <Sphere
-                numberOfSpheres={200}
+                numberOfSpheres={numberOfSpheres}
                 dimensions={dimensions}
                 wallsGroup={wallsGroup}
               />
